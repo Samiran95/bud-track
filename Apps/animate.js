e8d69581e9807c694221b202b1f2cebdc5921339
Samiran95 <132401863+Samiran95@users.noreply.github.com>
2023-05-19 19:17:02 +0530

    Add files via upload

--- Apps/animate.js
+++ Apps/animate.js
@@ -62,11 +62,35 @@ function leavingPage() {
         y: "-101vh",
         ease: Power4.easeOut,
     }),
+    
+    gsap.to(".apps-heading" ,{
+        y : -70,
+        duration : 1,
+    }) ,
+    
+    gsap.to(".lower-container" ,{
+        y : -70,
+        duration : 1,
+    })
 
-    gsap.to(".loaderleave" , {
+
+
+
+    gsap.to(".screen-name" ,{
+        y: -100,
+        opacity: 1,
+        duration : 0.5,
+        ease: Power2.easeOut
+    })
+    
+    gsap.to(".screen-name" ,{
+        y: -200,
         opacity: 0,
-        delay: 2
-    }),
+        duration : 0.5,
+        delay: 0.8,
+        ease: Power2.easeOut
+    
+    })
     
     gsap.to(".loaderenter" , {
         duration: 1,
@@ -75,9 +99,39 @@ function leavingPage() {
         delay : 1.3
     })
     
-    gsap.to(".lower-container" ,{
-        y : -50,
-        duration : 1,
+    //common Entry Animation
+    
+    
+    gsap.from(".apps-heading" ,{
+        opacity: 0,
+        y : 100,
+        duration : 0.5,
+        delay : 1.7
     })
+    
+    gsap.from(".lower-container" ,{
+        y : "101vh",
+        duration : 1,
+        delay : 1.9
+    })
+    
+    gsap.from (".lower-left>#container",{
+        y: "101vh",
+        duration : 1,
+        delay: 2.2,
+        stagger : 0.1,
+        duration: 0.9
+    })
+    
+    gsap.from (".lower-right>#container",{
+        y: "101vh",
+        duration : 1,
+        delay: 2.2,
+        stagger : 0.1,
+        duration: 0.9
+    })
+    
+
+
 }
 
