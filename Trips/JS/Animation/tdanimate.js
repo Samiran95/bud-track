8e26f88f6d87bb235aba77dcec8b1070928a1274
Samiran95 <132401863+Samiran95@users.noreply.github.com>
2023-05-22 20:22:59 +0530

    Add files via upload

--- Trips/JS/Animation/tdanimate.js
+++ Trips/JS/Animation/tdanimate.js
@@ -28,7 +28,7 @@ gsap.to(".loaderenter" , {
 
 gsap.from(".heading" , {
     duration: 1,
-    y: 200,
+    y: 100,
     ease: Power4.easeOut,
     delay : 1.8,
     opacity: 0
@@ -37,59 +37,60 @@ gsap.from(".heading" , {
 gsap.from(".trip-card" , {
     duration: 1,
     y: "101vh",
-    delay : 1.4,
+    delay : 1.8,
 })
 
 gsap.from(".trip-balance-word" , {
     duration: 1,
     y: "100",
-    delay : 1.5,
+    delay : 1.9,
     opacity: 0
 })
 
 gsap.from(".trip-balance-amount" , {
     duration: 1,
     y: "100",
-    delay : 1.65,
+    delay : 2.1,
     opacity: 0
 })
 
 gsap.from(".investment" , {
     duration: 1,
     y: "100",
-    delay : 1.8,
+    delay : 2.3,
     opacity: 0
 })
 
 gsap.from(".lower-button" , {
     duration: 1,
     y: "101vh",
-    delay : 1.5,
+    delay : 1.9,
 })
+
 
 gsap.from(".triplist-word" , {
     duration: 1,
     y: "101vh",
-    delay : 1.6,
+    delay : 2,
 })
 
 gsap.from(".trips-add" , {
     duration: 1,
     y: "101vh",
-    delay : 1.65,
+    delay : 2.05,
 })
 
 gsap.from(".triplist" , {
     duration: 1,
     y: "101vh",
-    delay : 1.75,
+    delay : 2.15,
 })
 
 gsap.from(".list-container" , {
     duration: 1,
     y: 300,
-    delay : 2,
-    stagger : 0.9
+    delay : 2.6,
+    stagger: 0.1
 
 })
 
